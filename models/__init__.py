95690ca46888903d2857a37d38784d885600a5f2
MeriemOurik-star <m.ourik4113@uca.ac.ma>
2026-05-09 19:33:35 +0200

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+from models.client import Client
+from models.humeur import Humeur
+from models.plat import Plat
+from models.commande import Commande
+from models.panier import Panier
+from models.face_scan import FaceScan
+
+__all__ = ["Client", "Humeur", "Plat", "Commande", "Panier", "FaceScan"]
